a108c79738d9a6d61cdaa0403f8f6b06d4c04b52
Nameless <tanisyun0331@icloud.com>
2022-06-18 23:10:47 +0900

    reviews api と categories api の実装

--- web-api/src/model/category.py
+++ web-api/src/model/category.py
@@ -1,6 +1,5 @@
 from db import Base
 from sqlalchemy import Column
-from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import String, BigInteger
 
 
@@ -8,3 +7,9 @@ class Category(Base):
     __tablename__ = 'categories'
     id = Column('id', BigInteger, primary_key=True, index=True)
     name = Column('name', String, nullable=False)
+
+    def toResultJSON(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
